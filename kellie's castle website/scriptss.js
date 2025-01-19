3db90e5a940f63591daad408fe5281411649c495
syirahnordin <syirahnordin2003@gmail.com>
2025-01-20 03:05:50 +0800

    Delete kellie's castle website directory

--- kellie's castle website/scriptss.js
+++
@@ -1,9 +0,0 @@
-const slides = document.querySelectorAll('.slides li');
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.previous');
-
-let currentSlide = 0;
-
-function moveSlide(dir) {
-    currentSlide += dir;
-    if (currentSlide < 0) currentSlide = 1-5
